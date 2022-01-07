5ce4471829e98f4ed21bf51601751be8f2950e1e
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2022-01-07 20:50:20 +0000

    Update docs configuration.

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pagesign'
-copyright = u'2021, Vinay Sajip'
+copyright = u'2021-%s, Vinay Sajip' %  datetime.date.today().year
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
